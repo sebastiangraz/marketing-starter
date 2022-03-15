9c54b895131b43a5c3eeeadaf726ed67808105d6
sebastiangraz <graz@live.se>
2022-03-15 16:51:58 +0100

    better parallax, better perf

--- components/modules/parallax.js
+++ components/modules/parallax.js
@@ -5,22 +5,28 @@ import {
   useTransform,
   transform,
   useSpring,
+  useMotionValue,
 } from "framer-motion";
 import { useRect } from "@reach/rect";
 
 const Parallax = ({ data = {} }) => {
   const { parallaxContainer } = data;
   const length = parallaxContainer.length;
-  const ref = useRef(),
-    childRef = useRef();
+  const ref = useRef();
   const [windowHeight, setWindowHeight] = useState(0);
-  const activeNavRect = useRect(ref, { observe: true });
+  // const activeNavRect = useRect(ref, { observe: true });
+  // const childRect = useRect(childRef, { observe: true });
+  //   console.log(activeNavRect && -(activeNavRect.top - window.innerHeight));
   const { scrollY } = useViewportScroll();
-
-  React.useEffect(() => {
-    setWindowHeight(window.innerHeight);
-  }, []);
-
+  const options = {
+    // ease: [[0.7, 0, 0.84, 0], [0.7, 0, 0.84, 0], [0.7, 0, 0.84, 0]]
+  };
+  //   const x = useTransform(
+  //     scrollY,
+  //     [activeNavRect && -(activeNavRect.top - window.innerHeight), 1000 * 3],
+  //     ["0px", `-${1000 * length}px`],
+  //     options
+  //   );
   const settings = {
     springOptions: {
       damping: 12,
@@ -28,24 +34,37 @@ const Parallax = ({ data = {} }) => {
     },
   };
 
-  const updatePos = (e) => {
-    let max = -(activeNavRect && activeNavRect.height);
-    let clamped = -Math.max(
-      0,
-      Math.min(-(activeNavRect && activeNavRect.top), -max - windowHeight)
-    );
-    return -transform(clamped, [0, max], [0, length]);
-  };
+  const x = useMotionValue(0);
+  const xSpring = useSpring(x, settings.springOptions);
 
-  const x = useSpring(
-    useTransform(scrollY, (e) => updatePos(e)),
-    settings.springOptions
-  );
+  React.useEffect(() => {
+    const el = ref && ref.current;
+    const elDistanceToTop = window.scrollY + el.getBoundingClientRect().top;
+    const elHeight = el.getBoundingClientRect().height - windowHeight;
+    const elWidth = el.getBoundingClientRect().width;
+    function updateX(e) {
+      const move = transform(
+        e,
+        [elDistanceToTop, elHeight + elDistanceToTop],
+        [0, -((length - 1) * (elWidth + 6))]
+      );
+      x.set(move);
+    }
+    const unsubscribeX = scrollY.onChange((e) => updateX(e));
+    return () => {
+      unsubscribeX();
+    };
+  }, [windowHeight]);
+
+  React.useEffect(() => {
+    setWindowHeight(window.innerHeight);
+  }, []);
 
   const style = {
     container: {
       height: `calc(${length} * 100vh)`,
       contain: "paint",
+      background: "#ff0",
     },
     innerContainer: {
       width: `calc(${length} * 100vw)`,
@@ -58,13 +77,16 @@ const Parallax = ({ data = {} }) => {
       borderRadius: "3rem",
       width: "100vw",
       height: "100vh",
-      background: "primary",
+      background: "text",
     },
     innerSection: {
       overflow: "hidden",
       variant: "layout.row",
     },
   };
+
+  console.log("render Parallax");
+
   return (
     <section
       sx={{
@@ -76,12 +98,12 @@ const Parallax = ({ data = {} }) => {
         <motion.div
           sx={style.innerContainer}
           style={{
-            x: `${x.current * 100}vw`,
+            x: xSpring,
           }}
         >
           {parallaxContainer.map((e) => {
             return (
-              <div key={e.id} sx={style.section} ref={childRef}>
+              <div key={e.id} sx={style.section}>
                 <div sx={style.innerSection}>{e.heading}</div>
               </div>
             );
