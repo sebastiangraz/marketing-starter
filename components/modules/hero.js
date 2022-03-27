2c124982d8c4e61d80470c96ee495d3318930cb4
Sebastian Graz <graz@graz.local>
2022-03-27 17:26:42 +0200

    finish 1st animation

--- components/modules/hero.js
+++ components/modules/hero.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Themed, Flex } from "theme-ui";
 import { Width } from "../width";
-import Icon, { hero } from "../icon";
+import { hero } from "../icon";
 import NextLink from "next/link";
 import { motion } from "framer-motion";
 
@@ -80,21 +80,36 @@ const cursorVariant = {
   hidden: {
     y: 0,
     x: 0,
-    opacity: 0,
-    transition: {
-      when: "beforeChildren",
-    },
   },
   visible: {
-    y: [-30, 40, 25, 35],
-    x: [-200, -100, 0, 39],
-    opacity: 1,
+    y: [200, 200, 120, 90, 90, 200],
+    x: [-180, -180, -170, -110, -110, -180],
     transition: {
       repeat: Infinity,
-      repeatType: "reverse",
+      repeatType: "loop",
+      repeatDelay: 4,
       type: "spring",
-      duration: 5,
-      when: "beforeChildren",
+      duration: 3,
+      delay: 4,
+    },
+  },
+};
+
+const notificationVariant = {
+  hidden: {
+    y: -8,
+    opacity: 0,
+  },
+  visible: {
+    y: [8, 0, 0, 0, -8],
+    opacity: [0, 1, 1, 1, 0],
+    transition: {
+      repeat: Infinity,
+      repeatType: "loop",
+      repeatDelay: 4,
+      type: "spring",
+      duration: 3,
+      delay: 4,
     },
   },
 };
@@ -159,7 +174,7 @@ const Hero = ({ data }) => {
             aria-labelledby="hero-"
             className="css-mbumtg-Hero"
             initial="hidden"
-            whileInView="visible"
+            animate="visible"
             variants={parentVariant}
           >
             <motion.path
@@ -287,7 +302,96 @@ const Hero = ({ data }) => {
               d={hero.amsterdamCard}
               fill="currentColor"
             />
+
             <foreignObject width="100%" height="100%">
+              <motion.div
+                variants={notificationVariant}
+                sx={{
+                  width: "210px",
+                  top: "309px",
+                  position: "relative",
+                  left: "160px",
+                }}
+              >
+                <svg
+                  // width="189"
+                  // height="42"
+                  // viewBox="0 0 189 42"
+                  fill="none"
+                  overflow="auto"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <rect
+                    x="0.5"
+                    y="4.5"
+                    width="183"
+                    height="37"
+                    rx="18.5"
+                    fill="white"
+                    stroke="currentColor"
+                  />
+                  <motion.rect
+                    initial={{ opacity: 0, scale: 1 }}
+                    animate={{
+                      opacity: [0.2, 0.2, 0.2, 0.2, 0.1],
+                      scale: [1, 1, 1, 1.2, 1.8],
+                    }}
+                    transition={{
+                      repeat: Infinity,
+                      repeatType: "loop",
+                      repeatDelay: 4,
+                      type: "spring",
+                      duration: 3,
+                      delay: 4,
+                    }}
+                    x="9"
+                    y="14"
+                    width="18"
+                    height="18"
+                    rx="9"
+                    fill="currentColor"
+                  />
+                  <motion.rect
+                    initial={{ opacity: 0, scale: 1 }}
+                    animate={{
+                      opacity: [0.5, 0.5, 0.5, 1, 0],
+                      scale: [1, 1, 1, 1.2, 0],
+                    }}
+                    transition={{
+                      repeat: Infinity,
+                      repeatType: "loop",
+                      repeatDelay: 4,
+                      type: "spring",
+                      duration: 3,
+                      delay: 4,
+                    }}
+                    x="9"
+                    y="14"
+                    width="18"
+                    height="18"
+                    rx="9"
+                    fill="currentColor"
+                  />
+                  <path
+                    d="M15 23L17 25L21 21"
+                    stroke="#F6E9D9"
+                    strokeLinecap="round"
+                  />
+                  <path d={hero.notificationText0} fill="currentColor" />
+                  <rect
+                    x="173"
+                    y="2"
+                    width="14"
+                    height="14"
+                    rx="7"
+                    fill="currentColor"
+                  />
+                  <path
+                    d="M179.918 13H180.938C181.008 13 181.038 12.97 181.038 12.9V6.1C181.038 6.03 181.008 6 180.938 6H180.378C180.318 6 180.268 6.02 180.218 6.05L178.568 6.94C178.508 6.97 178.458 7.02 178.458 7.09V8.16C178.458 8.23 178.488 8.25 178.538 8.25C178.568 8.25 178.598 8.24 178.638 8.22L179.818 7.56V12.9C179.818 12.97 179.848 13 179.918 13Z"
+                    fill="#F6E9D9"
+                  />
+                </svg>
+              </motion.div>
               <motion.div
                 variants={cursorVariant}
                 xmlns="http://www.w3.org/1999/xhtml"
@@ -319,14 +423,14 @@ const Hero = ({ data }) => {
                       vectorEffect="non-scaling-stroke"
                       d="M31.6115 4.70781L5.70539 13.9043C3.88897 14.5491 3.94607 17.1374 5.78916 17.7015L13.1443 19.9526C13.6972 20.1218 14.1498 20.5221 14.3854 21.0502L20.4719 34.6922C21.2038 36.3327 23.5594 36.2515 24.1766 34.5645L34.1588 7.27974C34.7405 5.68984 33.2069 4.14145 31.6115 4.70781Z"
                       fill="#F6E9D9"
-                      stroke="#3C1F04"
+                      stroke="currentColor"
                       strokeLinejoin="round"
                     />
                     <path
                       vectorEffect="non-scaling-stroke"
                       d="M30.2157 1.77049L6.42009 11.7141C4.67666 12.4426 4.82264 14.9595 6.63862 15.4816L14.7947 17.8264C15.3736 17.9928 15.8467 18.4114 16.0825 18.9657L21.1866 30.9659C21.541 31.799 22.7317 31.77 23.045 30.9207L32.8632 4.30811C33.468 2.66894 31.8278 1.09685 30.2157 1.77049Z"
                       fill="white"
-                      stroke="#3C1F04"
+                      stroke="currentColor"
                       strokeLinejoin="round"
                     />
                   </motion.svg>
