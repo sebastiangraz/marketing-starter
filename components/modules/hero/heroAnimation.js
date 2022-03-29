0b8122e2b36836ed96821824c78411992d5effa0
Sebastian Graz <graz@graz.fritz.box>
2022-03-29 17:44:11 +0200

    safari 2nd cursor fix

--- components/modules/hero/heroAnimation.js
+++ components/modules/hero/heroAnimation.js
@@ -77,18 +77,6 @@ const liquidateVariant = {
   },
 };
 
-const flowVariant = {
-  hidden: {
-    offsetDistance: "0%",
-    // opacity: 0,
-    scale: 0.1,
-  },
-  visible: {
-    offsetDistance: "100%",
-    scale: [0.1, 1, 1, 1, 1, 0.1],
-  },
-};
-
 export const HeroAnimation = ({ loop }) => {
   switch (loop) {
     case "flow-4":
@@ -189,6 +177,7 @@ export const HeroAnimation = ({ loop }) => {
               height="37"
               rx="18.5"
               fill="white"
+              vectorEffect="non-scaling-stroke"
               stroke="currentColor"
             />
             <motion.rect
@@ -258,7 +247,7 @@ export const HeroAnimation = ({ loop }) => {
               }}
             >
               <motion.g variants={cursorCheckReviewVariant}>
-                <motion.path
+                <path
                   vectorEffect="non-scaling-stroke"
                   d="M31.6115 4.70781L5.70539 13.9043C3.88897 14.5491 3.94607 17.1374 5.78916 17.7015L13.1443 19.9526C13.6972 20.1218 14.1498 20.5221 14.3854 21.0502L20.4719 34.6922C21.2038 36.3327 23.5594 36.2515 24.1766 34.5645L34.1588 7.27974C34.7405 5.68984 33.2069 4.14145 31.6115 4.70781Z"
                   fill="#F6E9D9"
@@ -275,24 +264,13 @@ export const HeroAnimation = ({ loop }) => {
               </motion.g>
             </motion.g>
           </g>
-
-          {/* </motion.div>
-          </motion.div> */}
         </>
       );
 
     case "checkReview2":
       return (
-        <foreignObject width="100%" height="100%">
-          <motion.div
-            variants={liquidateVariant}
-            sx={{
-              width: "210px",
-              top: "420px",
-              position: "relative",
-              left: "490px",
-            }}
-          >
+        <>
+          <motion.svg y="420" x="490" variants={liquidateVariant}>
             <svg
               width="84"
               height="74"
@@ -308,6 +286,7 @@ export const HeroAnimation = ({ loop }) => {
                 height="70"
                 rx="16"
                 fill="white"
+                vectorEffect="non-scaling-stroke"
                 stroke="currentColor"
               />
               <motion.rect
@@ -327,6 +306,7 @@ export const HeroAnimation = ({ loop }) => {
                 d="M15 23L17 25L21 21"
                 stroke="#F6E9D9"
                 strokeLinecap="round"
+                vectorEffect="non-scaling-stroke"
               />
               <g transform="translate(15 15)">
                 <path d={heroPath.notificationText2} fill="currentColor" />
@@ -336,57 +316,45 @@ export const HeroAnimation = ({ loop }) => {
                 fill="#F6E9D9"
               />
             </svg>
-          </motion.div>
-          <motion.div
-            variants={cursorLiquidateVariant}
-            xmlns="http://www.w3.org/1999/xhtml"
-            sx={{
-              position: "absolute",
-              top: "30%",
-              left: "70%",
-              width: "40px",
-              height: "50px",
-            }}
-          >
-            <motion.div
-              sx={{
-                offsetRotate: "0deg",
-                offsetPath: `path(
-                "M12.68 9.85c.56-2.5.72-7.27-2.81-8.31C5.04.1-.94 1.44 1.61 6.72c2.55 5.27 7.2 6.35 12.39-.63"
-                )`,
-              }}
-              initial={{ offsetDistance: "0%", scale: 1 }}
-              animate={{ offsetDistance: "100%", scale: 1 }}
+          </motion.svg>
+          <g transform="translate(585, 295)">
+            );
+            <motion.g
+              initial={{ "--offset": "0%" }}
+              animate={{ "--offset": "100%" }}
               transition={{
                 duration: 4,
                 repeat: Infinity,
                 repeatType: "mirror",
                 ease: "easeInOut",
               }}
+              sx={{
+                offsetDistance: "var(--offset)",
+                offsetRotate: "0deg",
+                offsetPath: `path(
+                "M12.68 9.85c.56-2.5.72-7.27-2.81-8.31C5.04.1-.94 1.44 1.61 6.72c2.55 5.27 7.2 6.35 12.39-.63"
+                )`,
+              }}
             >
-              <motion.svg
-                width="36"
-                height="40"
-                viewBox="0 0 36 40"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
+              <motion.g variants={cursorLiquidateVariant}>
                 <path
                   d="M4.00959 4.70781L29.9157 13.9043C31.7321 14.5491 31.675 17.1374 29.8319 17.7015L22.4768 19.9526C21.9239 20.1218 21.4713 20.5221 21.2357 21.0502L15.1492 34.6922C14.4173 36.3327 12.0617 36.2515 11.4445 34.5645L1.46227 7.27974C0.880596 5.68984 2.41417 4.14145 4.00959 4.70781Z"
                   fill="#F6E9D9"
+                  vectorEffect="non-scaling-stroke"
                   stroke="currentColor"
                   strokeLinejoin="round"
                 />
                 <path
                   d="M5.40536 1.77049L29.201 11.7141C30.9444 12.4426 30.7985 14.9595 28.9825 15.4816L20.8264 17.8264C20.2475 17.9928 19.7744 18.4114 19.5386 18.9657L14.4345 30.9659C14.0801 31.799 12.8894 31.77 12.5761 30.9207L2.75785 4.30811C2.15311 2.66894 3.79328 1.09685 5.40536 1.77049Z"
                   fill="white"
+                  vectorEffect="non-scaling-stroke"
                   stroke="currentColor"
                   strokeLinejoin="round"
                 />
-              </motion.svg>
-            </motion.div>
-          </motion.div>
-        </foreignObject>
+              </motion.g>
+            </motion.g>
+          </g>
+        </>
       );
     default:
       return <></>;
